bb445e80c81c762e1361f59d5def2210489a8335
MigueSilv4 <silvamedinamiguel@gmail.com>
2024-08-16 10:50:20 -0500

    create message table and add register button with redirect to view

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,21 @@
 module.exports = {
   content: ["./*"],
   theme: {
-    extend: {},
+    extend: {
+
+      w: {
+        '24': '6rem',    // Añade un valor de ancho menor
+        '32': '8rem',    // Puedes añadir otros valores personalizados si es necesario
+        '48': '12rem',
+        '64': '16rem',
+        '80': '20rem',
+        '96': '24rem',   // Valor mayor por defecto en Tailwind
+        // Puedes añadir valores menores aquí
+      }
+    },
   },
+
+  
   plugins: [],
 }
 
