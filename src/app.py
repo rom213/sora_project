7274174d996f7982b3a985ba2b41ea0143001c76
MigueSilv4 <silvamedinamiguel@gmail.com>
2024-09-29 20:25:52 -0500

    Add new changes ultimate roro

--- src/app.py
+++ src/app.py
@@ -68,4 +68,4 @@ register_socketio_events(socketio)
 if __name__ == "__main__":
     with app.app_context():
         db.create_all()  # Crea las tablas si no existen
-    socketio.run(app, host="0.0.0.0", port=5006, debug=True)
+    socketio.run(app, host="0.0.0.0", port=5000, debug=True)
