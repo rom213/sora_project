31afd728e136e41c14d351b23efe66ed7146d43a
rom213 <122625150+rom213@users.noreply.github.com>
2024-09-29 21:50:52 -0500

    config production

--- src/config.py
+++ src/config.py
@@ -18,7 +18,7 @@ class DevelopmentConfig(Config):
     DEBUG = True
     MYSQL_HOST = "localhost"
     MYSQL_USER = "root"
-    MYSQL_PASSWORD = "1234567"
+    MYSQL_PASSWORD = "B=HNBaII:aV9"
     MYSQL_DB = "chat"
     SQLALCHEMY_DATABASE_URI = f"mysql+pymysql://{MYSQL_USER}:{MYSQL_PASSWORD}@{MYSQL_HOST}/{MYSQL_DB}?charset=utf8mb4"
 
---
+++ src/ecosystem.config.js
@@ -0,0 +1,20 @@
+
+module.exports = {
+  apps: [
+    {
+      name: "flask-app",
+      script: "gunicorn",
+      args: "--worker-class eventlet -w 1 -b 0.0.0.0:3000 app:app",
+      interpreter: "none",
+      env: {
+        FLASK_ENV: "production",
+        FLASK_DEBUG: "1",
+        SECRET_KEY: "tu_clave_secreta",
+        MYSQL_HOST: "localhost",
+        MYSQL_USER: "root",
+        MYSQL_PASSWORD: "B=HNBaII:aV9",
+        MYSQL_DB: "chat",
+      },
+    },
+  ],
+};
